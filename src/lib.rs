4c22e63351f585f1b35a5aebce08635b41caa902
Dawson Moore <dwmm127@gmail.com>
2024-08-22 22:04:28 -0500

    Initial

---
+++ src/lib.rs
@@ -0,0 +1,196 @@
+// Logging
+use log::{info, warn, error, debug, trace};
+use logos::{Logos, Lexer};
+use std::num::ParseIntError;
+
+// File reading/writing
+use std::fs;
+use std::io::{Write, BufReader, BufRead, Error, Lines};
+
+#[derive(Default, Debug, Clone, PartialEq)]
+pub enum LexingError {
+    InvalidInteger(String),
+    #[default]
+    NonAsciiCharacter,
+}
+
+/// Error type returned by calling lex.slice().parse() to u8
+impl From<ParseIntError> for LexingError {
+    fn from(err: ParseIntError) -> Self {
+        use std::num::IntErrorKind::*;
+
+        match err.kind() {
+            PosOverflow | NegOverflow => LexingError::InvalidInteger("overflow error".to_owned()),
+            _ => LexingError::InvalidInteger("unknown error".to_owned()),
+        }
+    }
+}
+
+#[derive(Logos, Debug, PartialEq)]
+#[logos(error = LexingError)]
+#[logos(skip r"[ \t\r\f]+")]
+pub enum Token {
+    #[token("module")]
+    Module,
+
+    #[token("endmodule")]
+    EndModule,
+
+    #[token("parameter")]
+    Parameter,
+
+    #[token("inout")]
+    Inout,
+
+    #[token("input")]
+    Input,
+
+    #[token("output")]
+    Output,
+
+    #[token("reg")]
+    Reg,
+
+    #[token("wire")]
+    Wire,
+
+    #[token("assign")]
+    Assign,
+
+    #[token("if")]
+    If,
+
+    #[token("else")]
+    Else,
+
+    #[token("begin")]
+    Begin,
+
+    #[token("end")]
+    End,
+
+    #[token("posedge")]
+    Posedge,
+
+    #[token("negedge")]
+    Negedge,
+
+    #[token("timescale")]
+    Timescale,
+
+    #[regex(r"\d+[np]s")]
+    Time,
+
+    #[token("#")]
+    Pound,
+
+    #[token("(")]
+    OpenParen,
+
+    #[token(")")]
+    CloseParen,
+
+    #[token("[")]
+    OpenBracket,
+
+    #[token("]")]
+    CloseBracket,
+
+    #[token("{")]
+    OpenBrace,
+
+    #[token("}")]
+    CloseBrace,
+
+    #[token("==")]
+    BEQ,
+
+    #[token("<")]
+    BLT,
+
+    #[token(">")]
+    BGT,
+
+    #[token("<=")]
+    BLTE,
+
+    #[token(">=")]
+    BGTE,
+
+    #[token("=")]
+    Equals,
+
+    #[token("-")]
+    Subtract,
+
+    #[token("+")]
+    Add,
+
+    #[token("*")]
+    Multiply,
+
+    #[token("/")]
+    Divide,
+
+    #[token("?")]
+    QMark,
+
+    #[token("!")]
+    EMark,
+
+    #[token(":")]
+    Colon,
+
+    #[token(";")]
+    Semicolon,
+
+    #[token(",")]
+    Comma,
+
+    #[token("`")]
+    BTick,
+
+    #[token("_")]
+    Underscore,
+
+    #[token("@")]
+    At,
+
+    #[token("\n")]
+    Newline,
+
+    #[regex(r"\d+'b\d+")]
+    BinaryValue,
+
+    #[regex(r"\d+'bz")]
+    HiZValue,
+
+    #[regex(r"//.*\n")]
+    Comment,
+
+    #[regex(r"[a-zA-Z]+")]
+    Word,
+
+    #[regex(r"[0-9]+", |lex| lex.slice().parse())]
+    Integer(u8),
+}
+
+pub struct Sim {
+    fpath: std::path::PathBuf,
+    contents: String,
+}
+
+pub fn read_sv_file(path: &std::path::PathBuf) -> Result<String, Error> {
+    let contents = fs::read_to_string(path)?;
+
+    let mut lex = Token::lexer(&contents);
+
+    for line in lex.into_iter() {
+        match line {
+            Ok(l) => info!("lexing result: {:?}", l),
+            Err(e) => error!("lexing error: {:?}", e)
+        }
+    }
+
+    Ok(contents)
+}
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+// Logging
+use chrono::Local;
+use std::io::Write;
+use env_logger::Builder;
+use log::{info, warn, error, debug, trace, LevelFilter};
+
+// Argument parsing
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// File input path
+    input_path:  std::path::PathBuf,
+
+    /// File output path
+    output_path: std::path::PathBuf,
+
+    /// Sets logging level (0 = off ... 4 = trace)
+    #[arg(short, long, default_value_t = LevelFilter::Info)]
+    log_level: LevelFilter,
+
+    /// Enables verbose file output
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    Builder::new()
+        .format(|buf, record| {
+                writeln!(buf,
+                    "{} [{}] - {}",
+                    Local::now().format("%Y-%m-%d %H:%M:%S"),
+                    record.level(),
+                    record.args()
+            )
+        })
+        .filter(None, args.log_level)
+        .init();
+
+    let input = sv_sim::read_sv_file(&args.input_path);
+
+    match input {
+        Err(e) => error!("encountered an error reading {:?}: '{}'", args.input_path, e),
+        Ok(_) => ()
+    }
+}
